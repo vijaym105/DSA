d04588f8922cabcdf2fca1ff445daa6b669c6875
vijaym105 <malusarevijay10@gmail.com>
2025-12-28 23:45:59 +0530

    Day1

---
+++ day1.js
@@ -0,0 +1,43 @@
+//Concatenation
+// string + string = string
+// let a = "hell";
+// let b = "nahh";
+// console.log(a + b); // O/p hellnahh
+
+//string + int = string 
+// let a = "pose"
+// let b = 69;
+// console.log(typeof(a +b)); // O/p pose69 , type = string
+
+//Arthematic
+// int + int = int
+// let temp1 = 10
+// let temp2 = 20;
+// console.log(temp1 + temp2); O/P 30
+
+// Relation between string and integer
+// let a = 10;
+// let b = 20;  //string + /int + string + int + string + int = string 
+// console.log("the sum of" + a +"and"+b+"is"+ a+b); // O/p the sum of10and20is1020
+
+// let a = 10;
+// let b = 20;
+// console.log(a+b + " is the sum of " + a + " and " + b);
+// O/p 30 is the sum of 10 and 20
+
+
+// prompt data type will always be string 
+// let prompt = require('prompt-sync')()
+// let pr = Number(prompt("Whats your age?"));
+// console.log(typeof(pr));
+
+// Swaping of two number
+// let a = 10;
+// let b = 20;
+
+// b = a;
+// let temp = a - b
+// temp = b
+// console.log(temp);
+
+
